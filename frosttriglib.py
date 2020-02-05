1af2a4645fee7f68255216914903ad4f5923620a
jdfrost18 <jdfrost@DESKTOP-Q8C18VE.localdomain>
2020-02-05 13:26:50 -0500

    Added more comments

--- frosttriglib.py
+++ frosttriglib.py
@@ -34,19 +34,22 @@ def fcos(x):
 #If converged to machine precision then break out of loop
                 if sold==s: break
         return s
-    
+#Tangent is just sine/cosine, so call those two functions    
 def ftan(x):
     s = fsin(x)/fcos(x)
     return s
 
+#Call in cosine function since secant is 1/cosine
 def fsec(x):
     s = 1/fcos(x)
     return s
 
+#Call sine function since cosecant is 1/sine
 def fcsc(x):
     s=1/fsin(x)
     return s
 
+#Contangent is cosine over sine, so we just call those above functions
 def fcot(x):
     s=fcos(x)/fsin(x)
     return s
