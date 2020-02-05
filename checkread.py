05fbd410a57991fe7924857a66fea7c77816fab0
Daniel Grin <dgrin@cr-stokes004.quaker.haverford.edu>
2020-02-05 14:26:19 -0500

    synced up class stuff

---
+++ checkread.py
@@ -0,0 +1,6 @@
+import numpy as np
+a=np.loadtxt("values.txt",float)
+
+#for i in range(4):
+#	a[i]=a[i]+3
+a+=3.
--- sinx_best_for_func.py
+++ sinx_best_for_func.py
@@ -9,7 +9,7 @@ def gsin(x):
 	s,sold = 0.e0,0.e0
 
 #Keep at most 10000 terms in the Taylor series
-	for i in range(10000):
+	for i in range(2):
 		sold=s
 		s+= float((((-1)**i) * (x**((2*i) + 1))))/float(math.factorial(((2*i) + 1)))
 #If converged to machine precision then break out of loop
