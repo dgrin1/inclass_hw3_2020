5630b85a378e9d742347f291fdef20e3cfe391b0
Nina Inman <BRYNMAWR\npinman@stokes004-08.quaker.haverford.edu>
2020-02-05 13:41:35 -0500

    with a while loop

--- inman_for.py
+++ inman_for.py
@@ -12,3 +12,10 @@ for i in range(1,iterations):
 	#i = i + 1
 
 print("The result is: ", sum)
+
+#s = map(cos, r)
+#t = list(map(cos,r)) actually make it a python list
+#r = [] Empty list
+#r.append(4.0) python adds 4.0 to the end of a list...
+
+#how to make an array
