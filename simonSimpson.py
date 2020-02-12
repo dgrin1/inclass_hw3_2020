7e4f14533088a95c31ad84ea87ddb9d68cb8652d
Simon Perales <sperales@stokes004-19.quaker.haverford.edu>
2020-02-12 14:17:37 -0500

    mostly fixed trapezoid and simpson with help of paul

--- simonSimpson.py
+++ simonSimpson.py
@@ -1,12 +1,17 @@
 import numpy as np
 
-step = input("say a step size: ")
-summ = 0
+step = input("say how many steps: ")
+sum1 = 0
+sum2 = 0
+h = float(2)/(step-1)
 xx = np.linspace(0,2, step)
-funn = (xx**4)-(2*xx)+1
+funn = ((xx**4)-(2*xx)+1)
 
 
-for x in range(1,step-1, 2):
-    summ += (2/(3*step))*(funn[x]+)
+for x in range(1,step, 2):
+    sum1 += funn[x]
+    
+for x in range(2,step, 2):
+    sum2 += funn[x]
 
-print(summ)
+print((h/3)*(funn[0]+funn[2]+4*sum1+2*sum2))
